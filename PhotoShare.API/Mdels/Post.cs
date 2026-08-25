961bcab2227536920b77ec7b204d34c01c133414
Titufrk <titufrk@gmail.com>
2026-08-25 13:17:06 +0600

    4: Add Post Entity

---
+++ PhotoShare.API/Mdels/Post.cs
@@ -0,0 +1,11 @@
+﻿namespace PhotoShare.API.Models
+{
+    public class Post
+    {
+        public Guid Id { get; set; }
+        public string Caption { get; set; }
+        public string ImageUrl { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
