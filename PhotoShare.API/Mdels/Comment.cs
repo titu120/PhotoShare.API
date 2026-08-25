944c2fffce8fb03c3c8af3d9c1bc0e40bc80e957
Titufrk <titufrk@gmail.com>
2026-08-25 13:19:22 +0600

    6: Add Comment Entity

---
+++ PhotoShare.API/Mdels/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace PhotoShare.API.Models
+{
+    public class Comment
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public Guid PostId { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
