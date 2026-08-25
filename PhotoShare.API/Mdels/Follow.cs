ebec3813f9c4afaa7b3d61b55bd92361ce2d2ba4
Titufrk <titufrk@gmail.com>
2026-08-25 13:20:14 +0600

    7: Add Follow Entity

---
+++ PhotoShare.API/Mdels/Follow.cs
@@ -0,0 +1,10 @@
+﻿namespace PhotoShare.API.Models
+{
+    public class Follow
+    {
+        public Guid Id { get; set; }
+        public Guid FollowerId { get; set; }
+        public Guid FollowingId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
