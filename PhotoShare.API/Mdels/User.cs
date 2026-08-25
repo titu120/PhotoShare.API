38992cb3cbe45d5e8bf20fe5bfc882d92f9fbe81
Titufrk <titufrk@gmail.com>
2026-08-25 13:16:02 +0600

    2: Add User Entity

---
+++ PhotoShare.API/Mdels/User.cs
@@ -0,0 +1,13 @@
+﻿namespace PhotoShare.API.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string? Bio { get; set; }
+        public string? ProfilePictureUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
