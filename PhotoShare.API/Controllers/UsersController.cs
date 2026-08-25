c71ece940f05b723e66dabea68639b1b81dd2c4a
Titufrk <titufrk@gmail.com>
2026-08-25 14:57:18 +0600

    21: Add UsersController

---
+++ PhotoShare.API/Controllers/UsersController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhotoShare.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+    }
+}
