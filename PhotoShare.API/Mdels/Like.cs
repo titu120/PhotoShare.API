cb398ed1c4a9bbae0743ab319c0637d76cc1a5e3
Titufrk <titufrk@gmail.com>
2026-08-25 13:18:04 +0600

    5: Add Like Entity

---
+++ PhotoShare.API/Mdels/Like.cs
@@ -0,0 +1,10 @@
+﻿namespace PhotoShare.API.Models
+{
+    public class Like
+    {
+        public Guid Id { get; set; }
+        public Guid PostId { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
