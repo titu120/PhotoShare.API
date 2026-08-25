c2b8cd4b2dff688b50994a3cc021d9368b12ee02
Titufrk <titufrk@gmail.com>
2026-08-25 13:31:06 +0600

    12: Create AppDbContext class

---
+++ PhotoShare.API/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhotoShare.API.Models;
+
+namespace PhotoShare.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Follow> Follows { get; set; }
+    }
+}
