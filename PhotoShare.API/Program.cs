7cda05f24978018cc017b21e87e2c83fce3e98f1
Titufrk <titufrk@gmail.com>
2026-08-25 13:36:25 +0600

    16: Program.cs এ DbContext কে Dependency Injection এ register করুন, connection string appsettings.json এ রাখুন

--- PhotoShare.API/Program.cs
+++ PhotoShare.API/Program.cs
@@ -1,23 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using PhotoShare.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Services যোগ করা
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// DbContext কে DI Container এ register করা
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Swagger UI (আগে install করেছিলেন)
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
