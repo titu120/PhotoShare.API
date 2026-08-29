e3164c817f9ec7b8813bdae7a29b82ca1d09e107
Titufrk <titufrk@gmail.com>
2026-08-29 19:15:33 +0600

    61: FollowController বানান

---
+++ PhotoShare.API/Controllers/FollowController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhotoShare.API.Data;
+
+namespace PhotoShare.API.Controllers
+{
+    // এই Controller এর সব URL শুরু হবে api/Follow দিয়ে
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FollowController : ControllerBase
+    {
+        // Database এর সাথে কথা বলার টুল
+        private readonly AppDbContext _context;
+
+        public FollowController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
