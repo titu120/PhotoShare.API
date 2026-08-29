d621cff76cf051dc165ec83aff0b18935c8381f1
Titufrk <titufrk@gmail.com>
2026-08-29 12:06:12 +0600

    34: PostsController বানান

---
+++ PhotoShare.API/Controllers/PostController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhotoShare.API.Data;
+
+namespace PhotoShare.API.Controllers
+{
+    // এই Controller এর সব URL শুরু হবে api/Posts দিয়ে
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        // Database এর সাথে কথা বলার টুল
+        private readonly AppDbContext _context;
+
+        // এই টুল Controller কে সরবরাহ করা হচ্ছে (constructor)
+        public PostsController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
